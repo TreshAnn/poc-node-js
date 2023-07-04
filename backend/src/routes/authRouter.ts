80caab10fd50785de236f16c093873ee4b8fb0cc
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-07-04 15:27:58 +0800

    chore: add routing for Cats API

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -2,7 +2,9 @@ import express from 'express'
 const router = express.Router()
 
 import { registerUser } from '../controllers/auth'
+import { CatsController } from '../cats'
 
 router.route('/register').post(registerUser)
+router.route('/cats').get(CatsController)
 
 export default router;
