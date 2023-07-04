f78bc16807b42fae23e659b9acd52c0074b45b52
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-07-04 10:57:40 +0800

    chore: remove extra whitespace to response message

--- backend/src/middleware/error-handler.ts
+++ backend/src/middleware/error-handler.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 
 const errorHandlerMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log(err)
-    res.status(500).json({ msg: 'There was an error '})
+    res.status(500).json({ msg: 'There was an error' })
 }
 
 export default errorHandlerMiddleware;
