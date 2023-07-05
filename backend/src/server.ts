7034d98195f02b42920089bd014b437611f2eddd
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-07-05 22:26:03 +0800

    fix: add fallback port if .env does not exist

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,11 +26,11 @@ app.use('/api/v1/auth', authRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URL)
+        await connectDB(process.env.MONGO_URL, process.env.MONGO_DBNAME)
         app.listen(port, () => {
             console.log(`Server is listening on port ${port}...`)
         })
