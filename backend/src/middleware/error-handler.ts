456d90584de1a80f9950b8ded9aba6cc2ef9e3a3
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-07-05 13:10:16 +0800

    chore: add middleware for error handling

--- backend/src/middleware/error-handler.ts
+++ backend/src/middleware/error-handler.ts
@@ -1,8 +1,34 @@
 import { Request, Response, NextFunction } from 'express'
+import { StatusCodes } from 'http-status-codes'
 
-const errorHandlerMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log(err)
-    res.status(500).json({ msg: 'There was an error' })
+interface myError extends Error {
+    code?: number,
+    keyValue: string,
+    errors: string,
+    statusCode: StatusCodes,
+}
+
+const errorHandlerMiddleware = (err: myError , req: Request, res: Response, next: NextFunction) => {
+
+    // console.log(err.message)
+    const defaultError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong, try again later'
+    }
+
+    if (err.name === 'ValidationError') {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST,
+        defaultError.msg = err.message
+        // defaultError.msg = Object.values(err.errors).map((item) => item.message).join(',')
+    }
+
+    // email already taken
+    if (err.code && err.code === 11000) {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST,
+        defaultError.msg = `${Object.keys(err.keyValue)} already in use`
+    }
+    // res.status(defaultError.statusCode).json({ msg: err })
+    res.status(defaultError.statusCode).json({ msg: defaultError.msg })
 }
 
 export default errorHandlerMiddleware;
