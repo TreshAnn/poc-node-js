2cf114a36f1d17a49a07ffa430a2507453dde4ab
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-07-05 13:14:20 +0800

    chore: hashing of password

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import validator from 'validator'
+import bcrypt from 'bcryptjs'
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -19,6 +20,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide password']
     }
+})
+
+UserSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 export default mongoose.model('User', UserSchema);
