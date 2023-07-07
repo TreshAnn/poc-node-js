8e876a0f6696e52996a452a2764d6f60121ef3eb
sirJosh <carmen.markjoshua23@gmail.com>
2023-07-07 09:29:31 +0800

    chore: add function for comparePassword

--- backend/src/utils/authUtils.ts
+++ backend/src/utils/authUtils.ts
@@ -4,3 +4,7 @@ export async function hashPassword(password: string){
     const salt = await bcrypt.genSalt(10)
     return bcrypt.hash(password, salt)
 }
+export async function comparePassword(candidatePassword: string, hashedPassword: string): Promise<boolean> {
+    const isMatch = await bcrypt.compare(candidatePassword, hashedPassword);
+    return isMatch;
+ }
