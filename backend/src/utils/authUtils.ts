8a8cd0e64eae4968ec24256c490b7e0b2cee4d6e
Trecia Ann Mustar <Trecia.Ann.T.Mustar@ph.ey.com>
2023-07-07 11:42:39 +0800

    chore: merge authUtils from feat-login to feat -authentication

--- backend/src/utils/authUtils.ts
+++ backend/src/utils/authUtils.ts
@@ -1,20 +1,18 @@
 import bcrypt from 'bcryptjs'
-import jwt, { Secret }  from 'jsonwebtoken'
+import jwt, { Secret }from 'jsonwebtoken'
 
 export async function hashPassword(password: string){
     const salt = await bcrypt.genSalt(10)
     return bcrypt.hash(password, salt)
 }
-<<<<<<< HEAD
 
 const secretKey: Secret = process.env.JWT_SECRET_KEY || 'secret_key';
 
 export async function generateToken(payload: any): Promise<string> {
     return jwt.sign(payload, secretKey, { expiresIn: '1d' });
 }
-=======
+
 export async function comparePassword(candidatePassword: string, hashedPassword: string): Promise<boolean> {
     const isMatch = await bcrypt.compare(candidatePassword, hashedPassword);
     return isMatch;
- }
->>>>>>> origin/feat-login
+  }
