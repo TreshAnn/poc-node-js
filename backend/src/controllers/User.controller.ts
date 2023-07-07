7c18a48062c6d8b7eed3686845aba922a241180f
Trecia Ann Mustar <Trecia.Ann.T.Mustar@ph.ey.com>
2023-07-07 11:44:56 +0800

    chore: merge user.controller from feat-login to feat-authentication

--- backend/src/controllers/User.controller.ts
+++ backend/src/controllers/User.controller.ts
@@ -2,20 +2,15 @@ import { Request, Response} from 'express';
 import * as userService from '../services/User.services';
 import { IUserRq } from '../models/User.interface';
 import { StatusCodes } from 'http-status-codes';
-import { comparePassword } from '../utils/authUtils';
-import { BadRequestError, UnAuthenticatedError } from 'errors';
 
 export const registerUser = async (req: Request, res: Response) => {
     const rq: IUserRq = req.body
 
     userService.validate(rq)
 
-<<<<<<< HEAD
     const {user, token} = await userService.createUser(rq)
     res.status(StatusCodes.CREATED).json({ user, token })
-=======
-    const user = await userService.createUser(rq)
-    res.status(StatusCodes.CREATED).json({ user })
+    
 }
 export const loginUser = async (req: Request, res: Response) =>{
   const rq: IUserRq =req.body
@@ -23,5 +18,4 @@ export const loginUser = async (req: Request, res: Response) =>{
   const login= await userService.loginUser(rq)
 
   res.status(StatusCodes.OK).json({login});
->>>>>>> origin/feat-login
 }
