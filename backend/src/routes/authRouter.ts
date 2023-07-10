53b4fdb1b85ffbf32e0f8df271e2bba157f09eba
Trecia Ann Mustar <Trecia.Ann.T.Mustar@ph.ey.com>
2023-07-10 08:40:02 +0800

    chore: add routing for updateUser and deleteUser

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 const router = express.Router()
 
-import { registerUser, loginUser } from '../controllers'
+import { registerUser, loginUser, updateUser, deleteUser } from '../controllers'
 
 // User Routes
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
+router.route('/users/:id').patch(updateUser)
+router.route('/users/:id').delete(deleteUser)
 
 export default router;
