a975c5635ba5007fab99bf995330933737dda3f7
KezTimple <TCIntern.TIMPLE@ph.ey.com>
2023-07-11 15:20:53 +0800

    chore: Add routing for getAllUsers API

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 const router = express.Router()
 
-import { registerUser, loginUser, updateUser, deleteUser } from '../controllers'
+import { registerUser, loginUser, updateUser, deleteUser, getAllUsers } from '../controllers'
 
 // User Routes
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/users/:id').patch(updateUser)
 router.route('/users/:id').delete(deleteUser)
+router.route('/users').get(getAllUsers)
 
 export default router;
