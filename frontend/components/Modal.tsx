ec4cae0681a1867f215c74ab9062e53264488ad9
Dan <danielle.bote@ph.ey.com>
2023-07-12 14:10:20 +0800

    [front-end] chore add user impl

---
+++ frontend/components/Modal.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import useSWRMutation from "swr/mutation";
+
+interface TModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  openToast: () => void;
+}
+
+interface TRegisterRq {
+  username: string;
+  email: string;
+  password: String;
+}
+
+const createUser = async (url: string, { arg }: { arg: TRegisterRq }) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(arg),
+  });
+
+  if (!res.ok) {
+    throw new Error("Error"); // Throw the error
+  }
+
+  return res.json();
+};
+
+export default function Modal({
+  closeModal,
+  isOpen,
+  openToast,
+}: TModalProps): JSX.Element {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+
+  const { trigger } = useSWRMutation("/api/v1/auth/register", createUser, {
+    onError: (error, key) => {
+      console.log(error);
+    },
+    onSuccess: (data) => {
+      closeModal();
+      openToast();
+    },
+  });
+
+  function handleCreateUser() {
+    trigger({
+      email: email,
+      password: password,
+      username: username,
+    });
+  }
+
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Add User
+                  </Dialog.Title>
+
+                  <div className="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="col-start-1 col-end-7">
+                      <label className="block text-sm font-medium leading-6 text-gray-900">
+                        Username
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          name="username"
+                          id="username"
+                          value={username}
+                          onChange={(e) => {
+                            setUsername(e.currentTarget.value);
+                          }}
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="col-start-1 col-end-7">
+                      <label className="block text-sm font-medium leading-6 text-gray-900">
+                        Email
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="email"
+                          name="email"
+                          id="email"
+                          value={email}
+                          onChange={(e) => {
+                            setEmail(e.currentTarget.value);
+                          }}
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="col-start-1 col-end-7">
+                      <label className="block text-sm font-medium leading-6 text-gray-900">
+                        Password
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="password"
+                          name="password"
+                          id="password"
+                          value={password}
+                          onChange={(e) => {
+                            setPassword(e.currentTarget.value);
+                          }}
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <button
+                    className="flex w-full justify-center mt-6 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={handleCreateUser}
+                  >
+                    Create
+                  </button>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
