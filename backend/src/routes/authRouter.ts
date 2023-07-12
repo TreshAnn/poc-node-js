7335a2e0616b48930f817d0019279bf1921985d3
markjoshuacarmen <mark.joshua.carmen@ph.ey.com>
2023-07-12 08:50:28 +0800

    chore: add authenticateUser in getAllUsers

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -3,11 +3,12 @@ const router = express.Router()
 
 import { registerUser, loginUser, updateUser, deleteUser, getAllUsers } from '../controllers'
 import authenticateUser from '../middleware/auth'
+import auth from '../middleware/auth'
 // User Routes
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/users/:id').patch(updateUser, authenticateUser)
 router.route('/users/:id').delete(deleteUser, authenticateUser)
-router.route('/users').get(getAllUsers)
+router.route('/users').get(getAllUsers, authenticateUser)
 
 export default router;
